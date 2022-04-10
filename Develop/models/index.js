cedbe2e7aaae743db0bdd005acb43b7061015012
pdavisDU <pdavisdu@gmail.com>
2022-04-10 10:15:47 -0600

    fixed models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
-
+//have to use hasmany
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  though: ProductTag,
+});
 
 module.exports = {
   Product,
